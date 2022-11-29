c05899d4e52189d20e66ff10f8c81d29e92a29e0
MeksulaM <mateusz.meksula@gmail.com>
2022-11-29 22:50:53 +0100

    timer logic - first attempt

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 const form = document.querySelector("form");
+const startPauseButton = document.getElementById("start-pause-button");
 
+let numberOfCircuits;
 let numberOfSets;
 let exerciseDuration;
 let restDuration;
 let restBetweenSets;
 const prepareTime = 10;
 
+let seconds = 4;
+let minutes = 0;
+let displaySeconds;
+let displayMinutes;
+let timerInterval;
+let timerStatus = false;
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  numberOfSets = form[0].value;
-  exerciseDuration = form[1].value;
-  restDuration = form[2].value;
-  restBetweenSets = form[3].value;
+  numberOfCircuits = form[0].value;
+  numberOfSets = form[1].value;
+  exerciseDuration = form[2].value;
+  restDuration = form[3].value;
+  restBetweenSets = form[4].value;
+});
+
+function measureTime() {
+  seconds--;
+  
+  displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+  displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  
+  document.getElementById(
+    "timer"
+    ).innerText = `${displayMinutes}:${displaySeconds}`;
+    
+    if (seconds === 0 && minutes === 0) window.clearInterval(timerInterval);
+    
+    if (seconds === 0 && minutes > 0) {
+      minutes--;
+      seconds = 60;
+    }
+  }
+
+startPauseButton.addEventListener("click", () => {
+  if (!timerStatus) {
+    timerInterval = window.setInterval(measureTime, 1000);
+    startPauseButton.innerText = "PAUSE";
+    timerStatus = true;
+  } else {
+    window.clearInterval(timerInterval);
+    startPauseButton.innerText = "START";
+    timerStatus = false;
+  }
 });
