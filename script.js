c3cb865368026764bf63d80696ca60ffe767a3e4
MeksulaM <mateusz.meksula@gmail.com>
2022-11-30 14:30:30 +0100

    timer logic pt.2

--- script.js
+++ script.js
@@ -6,14 +6,27 @@ let numberOfSets;
 let exerciseDuration;
 let restDuration;
 let restBetweenSets;
-const prepareTime = 10;
+let prepareTime = 10;
 
-let seconds = 4;
+let seconds;
 let minutes = 0;
 let displaySeconds;
 let displayMinutes;
 let timerInterval;
 let timerStatus = false;
+
+let values = {
+  numberOfCircuits: undefined,
+  numberOfSets: undefined,
+  exerciseDuration: undefined,
+  restDuration: undefined,
+  restBetweenSets: undefined,
+};
+
+let warmUpStatus = true;
+let exerciseStatus = false;
+let restStatus = false;
+let bigRestStatus = false;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,29 +36,94 @@ form.addEventListener("submit", (e) => {
   exerciseDuration = form[2].value;
   restDuration = form[3].value;
   restBetweenSets = form[4].value;
+
+  values.numberOfCircuits = numberOfCircuits;
+  values.numberOfSets = numberOfSets;
+  values.exerciseDuration = exerciseDuration;
+  values.restDuration = restDuration;
+  values.restBetweenSets = restBetweenSets;
 });
 
-function measureTime() {
-  seconds--;
-  
-  displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
-  displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  
-  document.getElementById(
-    "timer"
-    ).innerText = `${displayMinutes}:${displaySeconds}`;
-    
-    if (seconds === 0 && minutes === 0) window.clearInterval(timerInterval);
-    
-    if (seconds === 0 && minutes > 0) {
-      minutes--;
-      seconds = 60;
+function warmUp() {
+  prepareTime--;
+  if (prepareTime === 0) {
+    warmUpStatus = false;
+    exerciseStatus = true;
+  }
+  return prepareTime;
+}
+
+function exercise() {
+  exerciseDuration--;
+  if (exerciseDuration === 0) {
+    exerciseStatus = false;
+    restStatus = true;
+    restDuration = values.restDuration;
+
+    if (numberOfSets === 1) {
+      exerciseStatus = false;
+      bigRestStatus = true;
     }
   }
+  return exerciseDuration;
+}
+
+function rest() {
+  restDuration--;
+  if (restDuration === 0) {
+    restStatus = false;
+    exerciseStatus = true;
+    numberOfSets--;
+    exerciseDuration = values.exerciseDuration;
+  }
+  return restDuration;
+}
+
+function bigRest() {
+  restBetweenSets--;
+  if (restBetweenSets === 0 && numberOfCircuits > 1) {
+    bigRestStatus = false;
+    exerciseStatus = true;
+    numberOfCircuits--;
+    exerciseDuration = values.exerciseDuration;
+  } else if (restBetweenSets === 0 && numberOfCircuits === 1) {
+    bigRestStatus = false;
+    exerciseStatus = false;
+    restStatus = false;
+    warmUpStatus = false;
+    timerStatus = false;
+  }
+  return restBetweenSets--;
+}
+
+function measureTime() {
+  if (warmUpStatus) {
+    seconds = warmUp();
+  }
+
+  if (exerciseStatus) {
+    seconds = exercise();
+  }
+
+  if (restStatus) {
+    seconds = rest();
+  }
+
+  if (bigRestStatus) {
+    seconds = bigRest();
+  }
+
+  displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+  displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  document.getElementById(
+    "timer"
+  ).innerText = `${displayMinutes}:${displaySeconds}`;
+}
 
 startPauseButton.addEventListener("click", () => {
   if (!timerStatus) {
-    timerInterval = window.setInterval(measureTime, 1000);
+    timerInterval = window.setInterval(measureTime, 100);
     startPauseButton.innerText = "PAUSE";
     timerStatus = true;
   } else {
